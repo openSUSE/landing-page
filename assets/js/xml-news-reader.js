2d7ce125dcccd043ec342a29258b1ae6345f42b7
Cynthia Sanchez <csanchez@suse.com>
2015-06-18 10:46:42 +0200

    added: comment with the xml source

--- assets/js/xml-news-reader.js
+++ assets/js/xml-news-reader.js
@@ -2,7 +2,7 @@
 
 $(function () {
   $.ajax({
-    url: 'opensuse.xml',
+    url: 'opensuse.xml', //change to: https://news.opensuse.org/feed/ once the page is on the opensuse Server
     dataType: "xml",
     success: function(xmldata) {
       $(xmldata).find('item').each(function(){
