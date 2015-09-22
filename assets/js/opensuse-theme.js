eeb8ca08b3409869654563b5c00cfe17b8b57db8
Cynthia Sanchez <csanchez@suse.com>
2015-09-22 13:23:44 +0200

    Facebook token

--- assets/js/opensuse-theme.js
+++ assets/js/opensuse-theme.js
@@ -356,3 +356,21 @@ $(document).on("ready", function(){
     $("#contribute-to-opensuse").addClass('no-svg')
   }
 })
+
+// Facebook token
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '831347386978737',
+    xfbml      : true,
+    version    : 'v2.4'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
