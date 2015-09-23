eea538fd42df8082f15fb0dd90ca9de41e4e041e
Antoine Belvire <antoine.belvire@laposte.net>
2015-09-23 17:17:42 +0200

    Handle languages varieties If navigator.language is not available fallback to a more general language code (for example, "es_ES" -> "es"). If still not available, fallback to English.

--- assets/js/opensuse-theme.js
+++ assets/js/opensuse-theme.js
@@ -247,22 +247,37 @@ $(document).on("click", ".change-language", function() {
 //check if there is a langCookie in the browser
 $(document).on("ready", function(){
 
+  var languageCode;
   var selectedLanguageName;
 
   if (cookieLanguage === undefined) {
     try {
-      window.lang.change(navigator.language);
-      selectedLanguageName = $(".languages").find("[data-language-value='" + navigator.language + "']").html();
+      // try to use navigator.language
+      languageCode = navigator.language.replace("-","_");
+      window.lang.change(languageCode);
     }
     catch(err) {
-      // navigator.language not available, falling back to default
-      selectedLanguageName = "English";
+      // navigator.language is not available
+      if (navigator.language.length > 2) {
+        try {
+          // try with a more general string (for example, if navigator.language is "es-ES" then "es" is tried)
+          languageCode = navigator.language.substring(0,2);
+          window.lang.change(languageCode);
+        }
+        catch(err) {
+          languageCode = "en";
+        }
+      }
+      else {
+        languageCode = "en";
+      }
     }
   }
   else {
-    selectedLanguageName = $(".languages").find("[data-language-value='" + cookieLanguage + "']").html();
+    languageCode = cookieLanguage;
   }
 
+  selectedLanguageName = $(".languages").find("[data-language-value='" + languageCode + "']").html();
   $(".selected-language").html(selectedLanguageName);
 
 });
