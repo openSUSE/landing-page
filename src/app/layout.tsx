43272d17ba22928a2dfde376896d677d7bd9777e
pieterdd <pieterdd@users.noreply.github.com>
2025-04-20 21:45:57 +0200

    Add footer links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.scss";
+import styles from './layout.module.scss';
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -28,6 +29,24 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.variable} ${poppinsBold.variable}`}>
         {children}
+        <div className={styles.footerHolder}>
+          <div className="container">
+            <footer>
+              <ul className={styles.links}>
+                <li><a href="https://news.opensuse.org/">News</a></li>
+                <li><a href="https://doc.opensuse.org/">Documentation</a></li>
+                <li><a href="https://en.opensuse.org/Main_Page">Wiki</a></li>
+                <li><a href="https://software.opensuse.org/">Get software</a></li>
+                <li><a href="https://forums.opensuse.org/">Forums</a></li>
+                <li><a href="https://lists.opensuse.org/">Mailing lists</a></li>
+                <li><a href="https://shop.opensuse.org/">Merch store</a></li>
+                <li><a href="https://planet.opensuse.org/">Blogs</a></li>
+                <li><a href="mailto:press@opensuse.org">Press inquiries</a></li>
+                <li><a href="https://en.opensuse.org/openSUSE:Artwork_brand#Trademark">Trademark</a></li>
+              </ul>
+            </footer>
+          </div>
+        </div>
       </body>
     </html>
   );
