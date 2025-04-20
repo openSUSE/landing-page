5d78ad6631587eb587002260cd6e6ab91de6a62c
pieterdd <pieterdd@users.noreply.github.com>
2025-04-20 23:15:34 +0200

    Remove screenshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,6 @@ export default function Home() {
               </div>
               <h1>Free your desktop</h1>
             </div>
-            <div className={styles.screenshot}>
-              <Image width={600} height={375} src="/img/screenshots/kde-light.png" alt="Screenshot" unoptimized />
-            </div>
           </div>
         </div>
       </div>
