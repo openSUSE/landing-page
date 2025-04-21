13983d01ea710f7a5ae4ea4727b36ca40e2c7df4
pieterdd <pieterdd@users.noreply.github.com>
2025-04-21 11:44:37 +0200

    Pull out text links for reuse

--- src/app/components/topNav.tsx
+++ src/app/components/topNav.tsx
@@ -4,6 +4,15 @@ import { useEffect, useState } from 'react';
 import styles from './topNav.module.scss';
 import Image from 'next/image';
 
+function TextLinks() {
+    return <>
+        <a className={styles.textLink} href="https://get.opensuse.org/">Download</a>
+        <a className={styles.textLink} href="https://forums.opensuse.org/">Forums</a>
+        <a className={styles.textLink} href="https://en.opensuse.org/Main_Page">Wiki</a>
+        <a className={styles.textLink} href="https://doc.opensuse.org/">Docs</a>
+    </>;
+}
+
 export default function TopNav() {
     const [hasScrolled, setHasScrolled] = useState(false);
 
@@ -22,10 +31,7 @@ export default function TopNav() {
                 <a className={styles.logo} href="/">
                     <Image width={32} height={32} src="/img/monochrome/logo.svg" alt="openSUSE logo" />
                 </a>
-                <a className={styles.textLink} href="https://get.opensuse.org/">Download</a>
-                <a className={styles.textLink} href="https://forums.opensuse.org/">Forums</a>
-                <a className={styles.textLink} href="https://en.opensuse.org/Main_Page">Wiki</a>
-                <a className={styles.textLink} href="https://doc.opensuse.org/">Docs</a>
+                <TextLinks />
             </div>
         </div>
     </div>;
